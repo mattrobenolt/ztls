cdfa32173e273155f070435ee1feb89c6191eb2d
Matt Robenolt <m@robenolt.com>
2026-05-28 13:11:24 -0700

    build: add asm emit step for examples, asm inspection confirmed hardware crypto

--- build.zig
+++ build.zig
@@ -33,5 +33,18 @@ pub fn build(b: *std.Build) void {
         const run = b.addRunArtifact(exe);
         const step = b.step(b.fmt("example-{s}", .{name}), b.fmt("Run {s} example", .{name}));
         step.dependOn(&run.step);
+
+        const asm_step = b.step(b.fmt("example-{s}-asm", .{name}), b.fmt("Emit asm for {s} example", .{name}));
+        const asm_exe = b.addExecutable(.{
+            .name = name,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(b.fmt("examples/{s}.zig", .{name})),
+                .target = target,
+                .optimize = .ReleaseFast,
+                .imports = &.{.{ .name = "ztls", .module = mod }},
+            }),
+        });
+        _ = asm_exe.getEmittedAsm();
+        asm_step.dependOn(&asm_exe.step);
     }
 }
