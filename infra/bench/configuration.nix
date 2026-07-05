3f55495794c4df2a23b289611c1eb197a01bba77
Matt Robenolt <m@robenolt.com>
2026-07-05 11:37:30 -0700

    increase remote nix download buffer (#11)

--- infra/bench/configuration.nix
+++ infra/bench/configuration.nix
@@ -8,6 +8,7 @@
     "nix-command"
     "flakes"
   ];
+  nix.settings.download-buffer-size = 134217728;
 
   # Disable ASLR for stable benchmark addresses and fewer page-table variations.
   boot.kernel.sysctl."kernel.randomize_va_space" = 0;
@@ -17,7 +18,7 @@
   systemd.services.amazon-ssm-agent.enable = false;
   systemd.services.systemd-timesyncd.enable = false;
   systemd.services.systemd-oomd.enable = false;
-  systemd.services.serial-getty@ttyS0.enable = false;
+  systemd.services."serial-getty@ttyS0".enable = false;
   systemd.services.dhcpcd.enable = false;
 
   environment.systemPackages = with pkgs; [
